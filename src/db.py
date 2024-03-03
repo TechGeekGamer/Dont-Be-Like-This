17d54704d1cebe56414875ee58c89f43f8ac0310
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2024-03-02 18:05:40 -0800

    add files

--- src/db.py
+++ src/db.py
@@ -9,14 +9,27 @@ class MySQLModel(Model):
     class Meta:
         database = db
 
+
 class GuildSettings(MySQLModel):
     ServerID = BigIntegerField(primary_key=True)
     ChannelID = BigIntegerField()
     WebHookURL = CharField(null=True)
 
+
 class BotSettings(MySQLModel):
     BotToken = CharField()
 
 
+class ClownRecord(MySQLModel):
+    id = AutoField()
+    MessageID = BigIntegerField()
+    MessageChannelID = BigIntegerField()
+
+    ServerID = BigIntegerField()
+    UserID = BigIntegerField()
+
+    DateOfBeingClowned = DateTimeField()
+
+
 if __name__ == "__main__":
-  db.create_tables([GuildSettings, BotSettings], safe=True)
+    db.create_tables([GuildSettings, BotSettings, ClownRecord], safe=True)
